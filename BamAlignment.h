de3817f963a3ed45cc084414b2af078962010b61
mikaels <mikaels@9efb377e-2e27-44b9-b91a-ec4abb80ed8b>
2009-04-10 17:59:00 +0000

    Defined some of the non-standard data types for compatibility with Visual Studio.
    git-svn-id: svn+ssh://gene.bc.edu/home/subversion/Derek/BamTools/trunk@5 9efb377e-2e27-44b9-b91a-ec4abb80ed8b

--- BamAlignment.h
+++ BamAlignment.h
@@ -7,7 +7,18 @@
 #ifndef BAMALIGNMENT_H
 #define BAMALIGNMENT_H
 
+#ifdef WIN32
+typedef char                 int8_t;
+typedef unsigned char       uint8_t;
+typedef short               int16_t;
+typedef unsigned short     uint16_t;
+typedef int                 int32_t;
+typedef unsigned int       uint32_t;
+typedef long long           int64_t;
+typedef unsigned long long uint64_t;
+#else
 #include <stdint.h>
+#endif
 
 // C++ includes
 #include <string>
--- BamReader.h
+++ BamReader.h
@@ -44,10 +44,22 @@
 
 // C library includes
 #include <assert.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifdef WIN32
+typedef char                 int8_t;
+typedef unsigned char       uint8_t;
+typedef short               int16_t;
+typedef unsigned short     uint16_t;
+typedef int                 int32_t;
+typedef unsigned int       uint32_t;
+typedef long long           int64_t;
+typedef unsigned long long uint64_t;
+#else
+#include <stdint.h>
+#endif
 
 // BGZF library includes/defines
 #include "bgzf.h"

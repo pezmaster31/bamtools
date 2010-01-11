9a8d3e6e4305daff54da7e1626b7ab67629aae11
barnett <barnett@9efb377e-2e27-44b9-b91a-ec4abb80ed8b>
2010-01-11 19:10:15 +0000

    Added #include<cstdio> to BamAux.h to cover use of printf. Added AlignmentFlag setter methods, per request by Aaron.
    git-svn-id: svn+ssh://gene.bc.edu/home/subversion/Derek/BamTools/trunk@39 9efb377e-2e27-44b9-b91a-ec4abb80ed8b

--- BGZF.h
+++ BGZF.h
@@ -29,16 +29,16 @@
 #ifndef BAMTOOLS_TYPES
 #define BAMTOOLS_TYPES
 	#ifdef _MSC_VER
-			typedef char                 int8_t;
-			typedef unsigned char       uint8_t;
-			typedef short               int16_t;
-			typedef unsigned short     uint16_t;
-			typedef int                 int32_t;
-			typedef unsigned int       uint32_t;
-			typedef long long           int64_t;
-			typedef unsigned long long uint64_t;
+		typedef char                 int8_t;
+		typedef unsigned char       uint8_t;
+		typedef short               int16_t;
+		typedef unsigned short     uint16_t;
+		typedef int                 int32_t;
+		typedef unsigned int       uint32_t;
+		typedef long long           int64_t;
+		typedef unsigned long long uint64_t;
 	#else
-			#include <stdint.h>
+		#include <stdint.h>
 	#endif
 #endif // BAMTOOLS_TYPES
 

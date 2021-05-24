685e33706939859464d8b96e9d722cf5a0bdc367
David Seifert <SoapZA@users.noreply.github.com>
2021-05-24 12:08:37 +0200

    Re-enable visibility support

--- src/api/api_global.h
+++ src/api/api_global.h
@@ -10,12 +10,7 @@
 #ifndef API_GLOBAL_H
 #define API_GLOBAL_H
 
+#include "api/bamtools_api_export.h"
 #include "shared/bamtools_global.h"
-
-#ifdef BAMTOOLS_API_LIBRARY
-#define API_EXPORT BAMTOOLS_LIBRARY_EXPORT
-#else
-#define API_EXPORT BAMTOOLS_LIBRARY_IMPORT
-#endif
 
 #endif  // API_GLOBAL_H
